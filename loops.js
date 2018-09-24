2feba7dcab2a4297cd491b96e65706a456af7868
Mike Curtin <curtinmjc@gmail.com>
2018-09-24 20:06:56 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
-/*
+
 function forLoop(array) {
-  for (let i = 0; i < 25; i++) {
-    if (i === 1) {
+  for (var i = 0; i < 25; i++) {
+    if (i == 1) {
       array.push('I am 1 strange loop.');
     } else {
       array.push('I am ${i} strange loops.');
@@ -9,8 +9,8 @@ function forLoop(array) {
   }
   return array;
 }
-*/
 
+/*
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
@@ -21,7 +21,7 @@ function forLoop(array) {
   }
   return array;
 }
-
+*/
 /*
 function forLoop(array) {
 	for (let i = 0; i < 25; i++) {
