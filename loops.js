0085fa5a496dc787d5e490dd2c158b27df9b84e0
Mike Curtin <curtinmjc@gmail.com>
2018-09-24 16:48:15 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,8 @@
 function forLoop(array) {
   for(var i = 0; i < 25; i++) {
+    if (i ===1) {
+      array.push("I am ${i} strage loops");
+    }
     array.push("I am ${i} strage loops");
   }
   return array;
